4aefe1c1b547e14cc49e3d71b99358034f0ac2b0
Evan Thibodeau <evan.thibodeau@liferay.com>
2021-03-03 16:42:56 +0400

    Add release notes.

---
+++ release.sh
@@ -0,0 +1,5 @@
+hub release create \
+	--attach modules/site-se-site-initializer/build/libs/com.liferay.site.se.site.initializer-1.0.0.jar \
+	--message 'v0.0.1' \
+	--message 'Initial Release.' \
+	'v0.0.1'
